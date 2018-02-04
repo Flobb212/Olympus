5707b66435a548a8784df7eddde2301a04a77859
Thomas Fletcher-Lobb <thomas.fletcher-lobb@students.plymouth.ac.uk>
2018-02-04 18:02:50 +0000

    Revision of plan
    Fiddled with dunGen script but took a step back to assess strategy in better form.

--- Olympus/Assets/Scripts/DungeonGeneration.cs
+++ Olympus/Assets/Scripts/DungeonGeneration.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class DungeonGeneration : MonoBehaviour
 {
+    //Chooses which pool of enemies and items to pull from
+    public int floorPool;
 
-	// Use this for initialization
-	void Start ()
+    //Dynamically change the size of the floor and how many rooms it has
+    public Vector2 floorSize;
+    public int numberOfRooms;
+    
+    //Keep the rooms in an array to easily monitor their position
+    //Also keep a list of occupied spaces to avoid building over rooms
+    Room[,] rooms;
+    List<Vector2> occupied = new List<Vector2>();
+    
+    //public GameObject thisRoom;
+
+    private void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        Generate();
+    }
+
+    void Generate()
     {
-		
-	}
+        // 2:56 in vid
+    }
+
+
 }
