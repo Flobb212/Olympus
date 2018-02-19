47633261d2b8cf4e83e2421e1035078b71a0f07b
Thomas Fletcher-Lobb <thomas.fletcher-lobb@students.plymouth.ac.uk>
2018-02-19 19:20:13 +0000

    Spawners
    Battled with finding best setup for spawners.
    Ended up having one for enemies/bosses and another for obstacles.

---
+++ Olympus/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<GameObject> floor1Enemies;
+    public List<GameObject> floor2Enemies;
+    public List<GameObject> floor3Enemies;
+
+    // Get floor number from a game manager
+    private int floorNum = 0;
+
+    public void Spawn()
+    {
+        int rand = 0;
+
+        if (floorNum == 1)
+        {
+            rand = Random.Range(0, floor1Enemies.Capacity);
+
+            Instantiate(floor1Enemies[rand], this.transform.position, this.transform.rotation);
+        }
+        else if (floorNum == 2)
+        {
+            rand = Random.Range(0, floor2Enemies.Capacity);
+
+            Instantiate(floor2Enemies[rand], this.transform.position, this.transform.rotation);
+        }
+        else if (floorNum == 3)
+        {
+            rand = Random.Range(0, floor3Enemies.Capacity);
+
+            Instantiate(floor3Enemies[rand], this.transform.position, this.transform.rotation);
+        }
+    }
+}
---
+++ Olympus/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public List<GameObject> spawnObj;
+
+    public void Spawn()
+    {
+        int rand = Random.Range(0, spawnObj.Capacity);
+
+        Instantiate(spawnObj[rand], this.transform.position, this.transform.rotation);
+    }
+
+}
